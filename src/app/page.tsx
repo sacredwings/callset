0b4b67808d7698aaf650887b42301aa2e8f9e6a4
stronggeniusa@gmail.com <stronggeniusa@gmail.com>
2025-10-24 21:57:38 +0700

    Кнопки на начальной странице

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,35 @@
 import Link from 'next/link'
 import Style from "./style.module.sass";
 import AuthLogin from '@/components/auth/login'
+import {ServerAccountGet} from "@/components/functions/urlApi";
+import {cookies} from "next/headers";
 
-export default function Home() {
+export default async function Home() {
+    const account = await ServerAccountGet({cookies: await cookies()})
+
     return (
         <div className={Style.page}>
             <div className={Style.block}>
                 <h1>Мессенджер</h1>
 
                 <div className={Style.button}>
-                    <Link href={'/auth/login'} className="btn btn-dark">Вход</Link>
-                    <Link href={'/auth/reg'} className="btn btn-dark">Регистрация</Link>
+                    {account ? <>
+                        <Link href={`/users/${account._id}`} className="btn btn-dark">
+                            <i className="fa-regular fa-circle-user"></i>
+                            <br/>
+                            {account.login}
+                        </Link>
+                        <Link href={'/users'} className="btn btn-dark">
+                            <i className="fa-solid fa-users"></i>
+                            <br/>
+                            пользователи
+                        </Link>
+                    </> : <>
+                        <Link href={'/auth/login'} className="btn btn-dark">Вход</Link>
+                        <Link href={'/auth/reg'} className="btn btn-dark">Регистрация</Link>
+
+                    </>}
+
                 </div>
             </div>
         </div>
